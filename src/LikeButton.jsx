a7b9d65992a23d0c1d5974fb92467054053a19d7
applego <applegorillappa@gmail.com>
2020-12-01 00:38:05 +0900

    useEffect()メソッド ・ライフサイクルメソッドを代替できる ・Functional Componentでライフサイクルを使える ・コードをまとめられる ○機能ベース（何をやっているのか） ☓ときの流れのベース（ライフサイクルのメソッド毎）
    useEffect()の仕組み
    ・レンダー毎にuseEffect()内の処理が走る
    ・代替できるメソッドは以下
    　componentDidMount()
    　componentDidUpdate()
    　componentWillUnmount()
    
    パターン①レンダー毎
    ・基本の形
    ・useEffect()内にCallback関数を書く
    ・Callbackはレンダー毎に呼ばれる
    ・return するCallback関数はアンマウント時に呼ばれる。（クリーンアップ関数）
    
    パターン②マウント時のみ
    ・第二引数の配列内の値を前回レンダーと今回レンダーで比較
    →変更があればCallback関数を実行
    ・第二引数に空の配列を渡すと最初の1回（マウント時）のみ実行される
    
    パターン③マウント&アンマウント
    ・①と②の複合系
    ・通常のCallbackはマウント時のみ
    ・アンマウント時はreturn内のクリーンアップ関数が実行される
    
    パターン④特定のレンダー時
    ・マウント時に実行される
    ・limitの値が変わった時に実行される

--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const LikeButton = (props) => {
+  const [count, counter] = useState(0);
+  const [limit, release] = useState(true);
+
+  const countUp = () => {
+    counter(count + 1)
+  }
+
+  useEffect(() => {
+    console.log('render') //クリックのたびに
+    document.getElementById('counter').addEventListener('click', countUp)
+    if (count >= 10)
+      counter(0)
+    return () => {
+      console.log('UnMounting') //クリックのたびに
+      document.getElementById('counter').removeEventListener('click', countUp)
+    }
+  },[limit])  //第二引数に空の配列を渡すと一回しか増えない, limitが変わったときに実行
+
   return (
-    <button id={"counter"}>いいね数: {props.count}</button>
+    <>
+      <button id={"counter"}>いいね数: {count}</button>
+      <button onClick={() => release(!limit)}>もっといいねしたい!</button>
+    </>
   )
 }
 
